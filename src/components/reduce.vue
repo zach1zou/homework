5087599d64852a2b445a3f988176e2439ebb7b04
zach1zou <791522322@qq.com>
2022-06-28 21:26:32 +0800

    提交

---
+++ src/components/reduce.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+        <p @click="getSomeValues()">点击统计{{str}}每个字符出现的次数</p>
+        <ul v-for="(index,item) in arr" v-show="isshow">
+          <li>{{item}}出现的次数是{{index}}</li>
+          </ul>
+  </div>
+</template>
+<script>
+export default {
+  props: [],
+  components: {
+  },
+  data () {
+      return {
+        str: 'asadasfwedsaafrtrqr',
+        isshow: false,
+        arr:''
+    }
+  },
+  methods: {
+    getSomeValues() {
+      let arr = this.str.split('')
+      let count = arr.reduce((obj, index) => {
+        if (index in obj) {
+          obj[index]++
+        } else {
+          obj[index] = 1
+        }
+        return obj
+      }, {})
+      this.arr = count 
+      this.isshow = !this.isshow
+      
+    }
+
+  },
+  created () {
+  },
+  mounted () {
+  },
+  filters: {
+  },
+  computed: {
+  },
+  watch: {
+  },
+}
+</script>
+<style lang="less" scoped>
+
+</style>
