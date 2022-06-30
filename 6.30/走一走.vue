9bd8d42ad70198723fe060ddb25fdf91a83dece9
Miadern <465029437@qq.com>
2022-06-30 19:23:43 +0800

    更新了6.30作业

---
+++ 6.30/走一走.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <ul v-for="item in myArr" :key="item">
+      <li>{{ item }}</li>
+    </ul>
+    <button @click="btn">走一走</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      myArr: ["帅哥", "美女", "程序猿"],
+    };
+  },
+  methods: {
+    btn() {
+      this.myArr.push(this.myArr[0]);
+      this.myArr.shift();
+    },
+  },
+};
+</script>
