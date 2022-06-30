ae0437352c10d01fda12a7d1b883e83a57c2cb3b
zhangyu <2863680540@qq.com>
2022-06-30 19:40:20 +0800

    提交作业2

---
+++ src/4帅哥美女.vue
@@ -0,0 +1,26 @@
+<template>
+	<div id="app">
+		<ul>
+			<li v-for="item in myArr" :key="item">{{ item }}</li>
+		</ul>
+		<button @click="btn">走一走</button>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			myArr: ['帅哥', '美女', '程序猿'],
+		};
+	},
+	methods: {
+		btn() {
+			// 头部数据加入到末尾
+			this.myArr.push(this.myArr[0]);
+			// 再把头部的数据删除掉
+			this.myArr.shift();
+		},
+	},
+};
+</script>
---
+++ src/截取字符.vue
@@ -0,0 +1,33 @@
+<!-- http://www.baidu.com?name=zhangsan&age=18&sex=nan#
+解析出
+ {name: 'zhangsan',age: 18, sex: 'nan' } -->
+
+<template>
+	
+	<div></div>
+</template>
+
+<script>
+let str = 'http://www.baidu.com?name=zhangsan&age=nan&sex=nan#';
+function getUrl(str) {
+	console.log(str);
+	console.log(str.indexOf('?'));
+	console.log(str.indexOf('#'));
+	console.log(str.slice(21, 50));
+	console.log(str.slice(21, 50).split('&'));
+	var newStr = str.slice(str.indexOf('?') + 1, str.indexOf('#')).split('&'); // console.log(newStr);
+	const obj = {};
+	newStr.forEach((item) => {
+		console.log(item);
+		const [key, val] = item.split('=');
+		console.log([key, val]);
+		obj[key] = val;
+	});
+	return obj;
+}
+getUrl(str);
+console.log(getUrl(str));
+export default {};
+</script>
+
+<style></style>
