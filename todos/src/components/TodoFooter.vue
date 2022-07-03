a15cdc268d370e6d9a8930f02224fd7262e670ca
zhangyu <2863680540@qq.com>
2022-07-03 22:38:09 +0800

    提交作业

---
+++ todos/src/components/TodoFooter.vue
@@ -0,0 +1,54 @@
+<template>
+	<footer class="footer">
+		<span class="todo-count"
+			>剩余<strong>{{ count }}</strong></span
+		>
+		<ul class="filters">
+			<li>
+				<a
+					:class="{ selected: isSel == 'all' }"
+					@click="btn('all')"
+					href="javascript:;"
+					>全部</a
+				>
+			</li>
+			<li>
+				<a
+					:class="{ selected: isSel == 'no' }"
+					@click="btn('no')"
+					href="javascript:;"
+					>未完成</a
+				>
+			</li>
+			<li>
+				<a
+					:class="{ selected: isSel == 'yes' }"
+					@click="btn('yes')"
+					href="javascript:;"
+					>已完成</a
+				>
+			</li>
+		</ul>
+		<button class="clear-completed" @click="clear">清除已完成</button>
+	</footer>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			isSel: 'all', // 值为all全部, yes已完成, no未完成其中一种
+		};
+	},
+	props: ['count'],
+	methods: {
+		btn(val) {
+			this.isSel = val;
+			this.$emit('fliters', val);
+		},
+		clear() {
+			this.$emit('clear');
+		},
+	},
+};
+</script>
---
+++ todos/src/components/TodoMain.vue
@@ -0,0 +1,29 @@
+<template>
+	<ul class="todo-list">
+		<!-- completed: 完成的类名 -->
+		<li :class="{ completed: item.isDone }" v-for="item in arr" :key="item.id">
+			<div class="view">
+				<input class="toggle" type="checkbox" v-model="item.isDone" />
+				<label>{{ item.name }}</label>
+				<button class="destroy" @click="del(item.id)"></button>
+			</div>
+		</li>
+	</ul>
+</template>
+
+<script>
+export default {
+	props: {
+		arr: {
+			type: Array,
+			default: () => [],
+		},
+	},
+	methods: {
+		//点击事件传入id，子向父传值
+		del(id) {
+			this.$emit('del', id);
+		},
+	},
+};
+</script>
